d18c068ac915b2fadd1cc45eb0e805d6dc0a27f3
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-05-17 15:06:29 +0000

    fixed response for invalid input (empty string)

--- js/script.js
+++ js/script.js
@@ -11,18 +11,18 @@
  */
 
 function CalculateFactorial () {
-  // initializing variables for integer given by user and the factorial to 1
+  // initializing variables for integer given by user and the factorial to 1, also response to an empty string
   let integer = parseInt(document.getElementById('f-int').value)
   let factorial = 1;
   let counter = integer;
   let response = "";
   // response for when user did not enter an integer
-  if (isNaN(integer)) {
-    response = "Please enter a valid integer."
+  if (integer == 0) { 
+    response = "1"
   }
   // response for when user enters 0 as integer
-  else if (integer == 0) {
-    response = "1" 
+  else if (isNaN(integer)) {
+    response = "Please enter a valid integer."
   }  
   
   // else to start the loop
@@ -43,6 +43,11 @@ function CalculateFactorial () {
     } while (counter >= 1);
   }
     
-  // display list to user
-  document.getElementById('results').innerHTML = "!" + integer + " --> " + response + " = " + factorial
+  // display factorial to user, different answers depending on if user enters valid input or invalid input
+   if (isNaN(integer)) {
+    document.getElementById('results').innerHTML = response;
+   } 
+   else {
+    document.getElementById('results').innerHTML = "!" + integer + " --> " + response + " = " + factorial;
+   }
 }
